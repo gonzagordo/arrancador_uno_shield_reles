4a62dfae196c1bfe6f6ecbceb9ab58613365b182
gonzagordo <agustingonzaga77@gmail.com>
2016-10-18 21:21:22 +0200

    añadido paro automatico funcional pero de tiempo a ojo revisar eso    -creada funcion auxiliar parada_confirmada

--- automaticos.ino
+++ automaticos.ino
@@ -1,10 +1,29 @@
 //funciones que controlan las acciones automaticas
 
+bool parada_confirmada()
+	{
+	bool estado = false;
+	
+	if ( captador(rele_220) ) temporizador_parada++;
+	else temporizador_parada=0;
+	
+	if (temporizador_parada>=tiempo_parada) 
+		{
+		temporizador_parada=tiempo_parada;
+		estado=true;
+		}
+	return estado;
+	}
 
 bool paro_automatico(){
 	bool estado = false;
+	if (captador(selector_auto) && !captador(contacto_activacion) && !parada_confirmada() )
+		{
+		Serial.println( "paro automatico" );    //debug
+		estado = true;
+		}
 	return estado;
-	}
+}
 
 
 bool gas_automatico(){
--- configuracion.h
+++ configuracion.h
@@ -1,18 +1,21 @@
 // este es el archivo de configuracion
 
 
-				
+// entradas				
 #define pulsador_arranque 13	//verde
 #define pulsador_paro 12		//rojo
 #define pulsador_gas  11		//blanco
 #define selector_auto  10		//naranja_banco
 #define rele_220  9				//marron_blanco
 #define contacto_activacion  8	//azul
-
+//salidas
 #define paro 7
 #define marcha 6
 #define seguridad 5
 #define gas 4
+//tiempos de funcionamiento tiempo =valor *10 milisegundos
+int tiempo_parada=20;
 
-  
- 
+
+//varibles auxiliares
+int temporizador_parada = 0;
