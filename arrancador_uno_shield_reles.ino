54b7c4765d84f3bda5082cc3b122390a15c1fefc
gonzagordo <agustingonzaga77@gmail.com>
2016-10-18 19:24:11 +0200

    cambio de _conf.ino a configuracion.h mas flexible asi

--- arrancador_uno_shield_reles.ino
+++ arrancador_uno_shield_reles.ino
@@ -1,19 +1,9 @@
 //+++++++++arrancador con arduinouno y shield de reles 
 
 // +++++++++++++++AKI VAN INCLUDES NECESARIOS+++++++++++++++++++++++++++++++++++
+#include "configuracion.h"
 
 
-#define pulsador_arranque 13	//verde
-#define pulsador_paro 12		//rojo
-#define pulsador_gas  11		//blanco
-#define selector_auto  10		//naranja_banco
-#define rele_220  9				//marron_blanco
-#define contacto_activacion  8	//azul
-
-#define paro 7
-#define marcha 6
-#define seguridad 5
-#define gas 4
 
 void setup()
 {
---
+++ configuracion.h
@@ -0,0 +1,18 @@
+// este es el archivo de configuracion
+
+
+				
+#define pulsador_arranque 13	//verde
+#define pulsador_paro 12		//rojo
+#define pulsador_gas  11		//blanco
+#define selector_auto  10		//naranja_banco
+#define rele_220  9				//marron_blanco
+#define contacto_activacion  8	//azul
+
+#define paro 7
+#define marcha 6
+#define seguridad 5
+#define gas 4
+
+  
+ 
